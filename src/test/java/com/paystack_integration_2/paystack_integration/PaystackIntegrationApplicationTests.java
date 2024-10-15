8f34406b5847db4bd5a7969b7eb7673bca44099f
teeredtech <tolulope.oredein@redtechlimied.com>
2024-10-15 10:00:43 +0100

    initial commit

---
+++ src/test/java/com/paystack_integration_2/paystack_integration/PaystackIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paystack_integration_2.paystack_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaystackIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
